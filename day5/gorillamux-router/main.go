6079c9c7d5472e9d46d05ad5ca744b47257a6bb7
baski910 <baskar910@gmail.com>
2022-08-08 17:10:28 +0530

    custom router example

---
+++ day5/gorillamux-router/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter() // custom router using gorilla mux
+
+	r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		title := vars["title"]
+		page := vars["page"]
+
+		fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
+	})
+	log.Println("starting server at port 8000")
+	http.ListenAndServe(":8000", r)
+}
