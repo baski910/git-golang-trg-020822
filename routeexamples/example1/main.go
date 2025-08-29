302549b23a64aabbafc6d8b2728130d14dd51ccc
baski910 <42275121+baski910@users.noreply.github.com>
2025-08-29 10:56:16 +0530

    Update main.go

--- routeexamples/example1/main.go
+++ routeexamples/example1/main.go
@@ -71,6 +71,11 @@ func PostMethod(w http.ResponseWriter, r *http.Request) {
 func corsMiddleware(next http.Handler) http.Handler {
     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
         w.Header().Set("Access-Control-Allow-Origin", "*")
+		if r.Method == http.MethodOptions{
+			w.Header().Set("Access-Control-Allow-Methods","GET,POST")
+			w.Header().Set("Access-Control-Allow-Headers","content-type")
+			return
+		}
         next.ServeHTTP(w, r)
     })
 }
