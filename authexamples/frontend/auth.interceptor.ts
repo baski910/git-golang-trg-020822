1836b5c1d73b580fe6d19bfe2827685fcfe899a1
baski910 <42275121+baski910@users.noreply.github.com>
2025-08-29 16:24:53 +0530

    Create auth.interceptor.ts

---
+++ authexamples/frontend/auth.interceptor.ts
@@ -0,0 +1,13 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const token = localStorage.getItem('token')
+  console.log(`token string inside interceptor:${token}`)
+  if (token){
+    const cloned = req.clone({
+      headers: req.headers.set('Authorization', `Token ${token}`)
+    })
+    return next(cloned)
+  }
+  return next(req);
+};
